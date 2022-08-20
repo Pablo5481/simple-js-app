d19a4b518285a33b07a6e8103f0c79a47743db00
Pablo5481 <pablo.samuel.coronel@gmail.com>
2022-08-20 16:01:22 -0400

    added in let statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 alert('Hello world');
 let favoriteFood = 'banana';
 document.write(favoriteFood)
-test
-test
+let name = 'John';
+let welcomeMessage1 = "Hello, I'm";
+let welcomeMessage2 = 'and I say to you: "Hello!"';
