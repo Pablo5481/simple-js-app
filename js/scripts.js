18f13373861848b94c2bb5e6b332604ed383ac7f
Pablo5481 <pablo.samuel.coronel@gmail.com>
2022-08-23 23:53:07 -0400

    testing chapter 3 lesson

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,32 @@ const pokemonList = [
     { 
         name: 'Bulbasaur', 
         height: 4, 
-        types: [
+        types: 
             'grass', 
             'poison' 
-        ]
+        
     },
 
     { 
         name: 'Charmander', 
         height: 3, 
-        types: [
+        types: 
             'fire' 
-        ]
+        
     },
 
     { 
         name: 'Squirtle', 
         height: 2, 
-        types: [
+        types: 
             'water' 
-        ]
+        
     }
 ];
+
+let age = 21;
+if (age === 20) {
+    console.log('You are 20 years old!');
+} else {
+    console.log('You are not 20 years old');
+}
