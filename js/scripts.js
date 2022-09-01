d34d9f956afb114d703d60f1af571c67f479d98c
Pablo5481 <110000704+Pablo5481@users.noreply.github.com>
2022-09-01 13:55:46 -0400

    updated chapter 5 tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
+let pokemonRepository = (function() {
 const pokemonList = [
     { 
         name: 'Bulbasaur', 
         height: 4, 
+        weight: 6.9 + 'kg',
         types: [
             'grass', 
             'poison' 
@@ -11,6 +13,7 @@ const pokemonList = [
     { 
         name: 'Charmander', 
         height: 3, 
+        weight: 8.5 + 'kg',
         types: 
             'fire' 
         
@@ -19,16 +22,39 @@ const pokemonList = [
     { 
         name: 'Squirtle', 
         height: 2, 
+        weight: 9 + 'kg',
         types: 
             'water' 
         
     }
 ];
 
+
 pokemonList.forEach(function(pokemon){
 
-    document.write(pokemon.name + ' height: ' +  pokemon.height)
+    document.write(pokemon.name + ' height: ' +  pokemon.height + ', weight: ' + pokemon.weight)
     if (pokemon.height >3){
         document.write(' - Wow, that\'s huge!' + '<br/>');
     } else document.write('<br/>');
+
 });
+
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+function getAll() {
+    return pokemonList;
+}
+
+return {
+    add: add,
+    getAll: getAll,
+}
+
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
