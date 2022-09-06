4a5f5772b6399e0e230942ba8094c3e7c50344f4
Pablo5481 <110000704+Pablo5481@users.noreply.github.com>
2022-09-06 17:10:40 -0400

    updated chapter 6 task part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,60 @@
-let pokemonRepository = (function() {
-const pokemonList = [
-    { 
-        name: 'Bulbasaur', 
-        height: 4, 
-        weight: 6.9 + 'kg',
-        types: [
-            'grass', 
-            'poison' 
-        ]
-    },
-
-    { 
-        name: 'Charmander', 
-        height: 3, 
-        weight: 8.5 + 'kg',
-        types: 
-            'fire' 
-        
-    },
-
-    { 
-        name: 'Squirtle', 
-        height: 2, 
-        weight: 9 + 'kg',
-        types: 
-            'water' 
-        
-    }
-];
-
-
-pokemonList.forEach(function(pokemon){
-
-    document.write(pokemon.name + ' height: ' +  pokemon.height + ', weight: ' + pokemon.weight)
-    if (pokemon.height >3){
-        document.write(' - Wow, that\'s huge!' + '<br/>');
-    } else document.write('<br/>');
-
-});
-
-
-function add(pokemon) {
-    pokemonList.push(pokemon);
-}
-
-function getAll() {
-    return pokemonList;
-}
-
-return {
-    add: add,
-    getAll: getAll,
-}
-
-})();
-
-pokemonRepository.getAll().forEach(function (pokemon) {
+const pokemonRepository = (function() {
+    const repository = [
+      { 
+          name: 'Bulbasaur', 
+          height: 4, 
+          weight: 6.9 + 'kg',
+          types: [
+              'grass', 
+              'poison' 
+          ]
+      },
+  
+      { 
+          name: 'Charizard', 
+          height: 3, 
+          weight: 8.5 + 'kg',
+          types: 
+              'fire' 
+          
+      },
+  
+      { 
+          name: 'Squirtle', 
+          height: 2, 
+          weight: 9 + 'kg',
+          types: 
+              'water' 
+          
+      },
+  ];
+  
+  function add(pokemon) {
+      repository.push(pokemon);
+  }
+  
+  function getAll() {
+      return repository;
+  }
+  
+  function addListItem(pokemon){
+      const pokemonList = document.querySelector(".pokemon-list");
+      const listPokemon = document.createElement("li");
+      const button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      listPokemon.appendChild(button);
+      pokemonList.appendChild(listPokemon);
+  }
+  
+  return {
+      add: add,
+      getAll: getAll,
+      addListItem: addListItem
+  };
+   
+  })();
+  
+  pokemonRepository.getAll().forEach(function (pokemon){    
     pokemonRepository.addListItem(pokemon);
-  });
+  }); 
